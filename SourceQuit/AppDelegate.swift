87c10e714323cd4f6c73421bfbd7ac3ada47ad9b
Tamas Lustyik <elveestei@gmail.com>
2017-11-24 22:34:27 +0100

    Basic menu bar app

--- SourceQuit/AppDelegate.swift
+++ SourceQuit/AppDelegate.swift
@@ -11,17 +11,55 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    private var statusItem: NSStatusItem!
 
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let statusBar = NSStatusBar.system
+        statusItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+        
+        statusItem.image = NSImage(named: NSImage.Name(rawValue: "statusicon"))
+        statusItem.image?.isTemplate = true
+        
+        NSMenu.setMenuBarVisible(false)
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let menu = NSMenu(title: statusItem.title!)
+        let menuItems = buildMenuItems()
+        
+        for item in menuItems {
+            menu.addItem(item)
+        }
+        
+        statusItem.menu = menu
+    }
+    
+    private func buildMenuItems() -> [NSMenuItem] {
+        var items: [NSMenuItem] = []
+        
+        items.append(NSMenuItem(title: "Kill com.apple.dt.SKAgent",
+                                action: #selector(killSKAgent(_:)),
+                                keyEquivalent: ""))
+        items.append(NSMenuItem(title: "Kill SourceKitService",
+                                action: #selector(killSourceKitService(_:)),
+                                keyEquivalent: ""))
+        items.append(.separator())
+        items.append(NSMenuItem(title: "Quit SourceQuit",
+                                action: #selector(quit(_:)),
+                                keyEquivalent: ""))
+        
+        return items
+    }
+    
+    @objc private func killSKAgent(_ sender: Any) {
+        Process.launchedProcess(launchPath: "/usr/bin/pkill", arguments: ["-9", "com.apple.dt.SKAgent"])
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    @objc private func killSourceKitService(_ sender: Any) {
+        Process.launchedProcess(launchPath: "/usr/bin/pkill", arguments: ["-9", "SourceKitService"])
     }
 
+    @objc private func quit(_ sender: Any) {
+        NSApplication.shared.terminate(nil)
+    }
 
 }
 
